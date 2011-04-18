5a8eb484523ac38fcf1bf86ab17076a8049f7601
Rocky Burt <rocky@serverzen.com>
2011-04-18 12:23:52 -0230

    Removed legacy files

--- khufu_templview/__init__.py
+++
@@ -1,7 +0,0 @@
-import khufu_templview.templateview
-
-
-def includeme(config):
-    config.include('pyramid_jinja2')
-    config.add_directive('add_templateview_route',
-                         khufu_templview.templateview.add_templateview_route)
